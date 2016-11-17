1c27a8a94a3c6db63127eb387ed903475ba8ea42
CcSunshine <764683559@qq.com>
2016-11-17 19:56:24 +0800

    对riskitem保存时，追踪者列表的修改

--- src/main/java/edu/rms/businessImpl/RiskItemBusinessImpl.java
+++ src/main/java/edu/rms/businessImpl/RiskItemBusinessImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import edu.rms.business.RiskItemBusiness;
 import edu.rms.dao.RiskItemDao;
+import edu.rms.dao.UserDao;
 import edu.rms.model.RiskItem;
 
 
@@ -15,10 +16,18 @@ import edu.rms.model.RiskItem;
 public class RiskItemBusinessImpl implements RiskItemBusiness{
 	@Autowired
 	private RiskItemDao riskItemDao;
-	
+	private UserDao udao;
 	
 	@Override
 	public String addRiskItem(RiskItem riskItem) {
+		String[] temp=riskItem.getTracker_id().split(";");
+		String trackername="";
+		for(int i=0;i<temp.length;i++){
+			String a=udao.getById(Integer.parseInt(temp[i])).getUser_name();
+			trackername=trackername+a+";";
+		}
+		trackername=trackername.substring(0,trackername.length()-1);
+		riskItem.setTracker_name(trackername);
 		return riskItemDao.save(riskItem);
 	}
 
@@ -37,7 +46,7 @@ public class RiskItemBusinessImpl implements RiskItemBusiness{
 		String[] trs=null;
 		for(int i=0;i<temp.size();i++){
 			tr=temp.get(i).getTracker_id();
-			trs=tr.split(",");
+			trs=tr.split(";");
 			for(int j=0;j<trs.length;j++){
 				if(trs[j].equals(userid)){
 					re.add(temp.get(i));
